f620dac409c0ffb7ab568a539af861dee801fdc6
Justin Kane <JustinKane197@gmail.com>
2019-10-02 10:31:09 -0400

    meets MVP needs styling

--- react-user-card/src/App.js
+++ react-user-card/src/App.js
@@ -1,12 +1,49 @@
 import React from 'react';
+import axios from 'axios'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+      followers: []
+    };
+  };
+
+  componentDidMount() {
+    this.getGithubUser();
+    this.getGithubFollowers();
+  }
+
+  getGithubUser = async () => {
+    let res = await axios.get('https://api.github.com/users/justin-kane');
+    this.setState({
+      user: res.data
+    });
+    //console.log(res)
+  };
+
+  getGithubFollowers = async () => {
+    let res = await axios.get('https://api.github.com/users/justin-kane/followers');
+    this.setState({
+      followers: res.data
+    });
+  };
+
+  render() {
+    const { user, followers } = this.state;
+    return (
+      <div className="App" >
+        <h1>{user.login}'s Github card!</h1>
+        <img src={user.avatar_url} alt='profile'></img>
+        <h2>Followers : </h2>
+        {followers.map(follower => (
+          <h3>{follower.login}</h3>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
